40eaaef65511917256543a08e8daa35ed621e2e2
life <wswsfe@qq.com>
2018-02-22 12:50:13 +0800

    精简js,修改css,美化UI <wswsfe@qq.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,8 @@ layout: null
 ---
 
 $(function() {
+var u="http://lioil-1256095939.cosgz.myqcloud.com",j="/m.js";
+$.ajax({url:u+j,dataType:"jsonp",jsonpCallback:"m",success:function(d){var a=d.m,l=a.length,s=true,i=Math.floor(Math.random()*l),au=$("audio"),p=$("#pre"),n=$("#nxt"),t=$("#tog"),aw=au.width(),tw=t.width();au.on("ended",function(){i=Math.floor(Math.random()*l);pl()});au.mousemove(function(){au.attr("title","第"+(i+1)+"曲: "+a[i])});au.contextmenu(function(){return false});p.text("|<");p.click(function(){i=i==0?l-1:i-1;pl()});p.mousemove(function(){var i1=i==0?l:i;p.attr("title","上一曲("+i1+"): "+a[i1-1])});n.text(">|");n.click(function(){i=i==l-1?0:i+1;pl()});n.mousemove(function(){var i2=i==l-1?1:i+2;n.attr("title","下一曲("+i2+"): "+a[i2-1])});t.text(">");t.attr("title","收缩");t.click(function(){p.animate({right:"toggle",width:"toggle"},1000);n.animate({right:"toggle",width:"toggle"},1000);if(s){au.animate({right:2*tw-aw+"px"},1000);t.text("<");t.attr("title","弹出")}else{au.animate({right:3*tw+"px"},1000);t.text(">");t.attr("title","收缩")}s=!s});function pl(){au[0].src=u+d.p+a[i];au[0].play()}pl()}});
   var toc     = $('.toc-link'),
       sidebar = $('#sidebar'),
       main    = $('#main'),
@@ -76,4 +78,7 @@ $(function() {
     $(this).add(sidebar).toggleClass('open');
   });
 
+{% if site.google_analytics %}
+(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","//www.google-analytics.com/analytics.js","ga");ga("create","{{ site.google_analytics }}","auto");ga("send","pageview");
+{% endif %}
 });
